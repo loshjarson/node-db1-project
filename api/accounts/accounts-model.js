91eeb63e6991bead77d5fa5912334aef4d1ab319
unknown <jlarson129@outlook.com>
2021-12-15 11:13:08 -0500

    model functions

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,28 @@
+const db = require("../../data/db-config")
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db("accounts")
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return await db("accounts").where("id",id).first()
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+  const [ id ] = await db("accounts").insert(account);
+  return getById(id)
 }
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
+  await db("accounts").where("id",id).update(account);
+  console.log(getById(id));
+  return getById(id)
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  const deleted = await getById(id)
+  await db("accounts").where("id",id).delete()
+  return deleted
 }
 
 module.exports = {
